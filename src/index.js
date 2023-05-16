efc3bc73027395ad383a4d2ab36dbcda0cc0d98d
Joseph Lurvey <joeylurvey@gmail.com>
2023-05-15 21:07:36 -0400

    complete challengemini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const taskDescription = document.getElementById('new-task-description').value;
+    const taskElement = document.createElement('li');
+    taskElement.textContent = taskDescription;
+    taskList.appendChild(taskElement);
+    //clear input field
+    document.getElementById('new-task-description').value = ""
+  });
 });
